2636a28984d60a4fbd351c9e1592f09e49905b6b
kk0139317 <kk4881757@gmail.com>
2024-06-03 09:09:47 +0530

    new file:   startaup_website/.pg_services.conf 	new file:   startaup_website/my_pages 	modified:   startaup_website/settings.py

--- startaup_website/settings.py
+++ startaup_website/settings.py
@@ -74,13 +74,22 @@ WSGI_APPLICATION = 'startaup_website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "service": "my_service",
+            "passfile": ".my_pgpass",
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
